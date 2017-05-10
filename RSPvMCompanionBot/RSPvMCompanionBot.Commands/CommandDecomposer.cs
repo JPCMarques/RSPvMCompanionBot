564d6e71cc232be964e7ac64c5c1d63e526f5f6a
JP Marques <JP Marques>
2017-05-10 13:50:17 +0100

    Improved upon the command logic.

---
+++ RSPvMCompanionBot/RSPvMCompanionBot.Commands/CommandDecomposer.cs
@@ -0,0 +1,39 @@
+﻿using RSPvMCompanionBot.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RSPvMCompanionBot.Utility
+{
+    public class CommandDecomposer
+    {
+
+        /*
+        public static ArgumentDataMatches ParseCommandInput(ArgumentData argumentData, out string input, bool optArgsFirst)
+        {
+            ArgumentDataMatches matches = new ArgumentDataMatches();
+            Dictionary<Regex, ICollection<string>> optArgsMatched, argsMatched;
+            if (optArgsFirst)
+            {
+                optArgsMatched = CommandRecognizer.Recognize(out input, argumentData.OptArgs.Values);
+                argsMatched = CommandRecognizer.Recognize(out input, argumentData.Args.Values);
+            }
+            else
+            {
+                argsMatched = CommandRecognizer.Recognize(out input, argumentData.Args.Values);
+                optArgsMatched = CommandRecognizer.Recognize(out input, argumentData.OptArgs.Values);
+            }
+        }*/
+
+        public static string RemoveMultipleSpaces(string input)
+        {
+            RegexOptions options = RegexOptions.None;
+            Regex regex = new Regex("[ ]{2,}", options);
+            return regex.Replace(input, " ");
+        }
+       
+    }
+}
---
+++ RSPvMCompanionBot/RSPvMCompanionBot.Commands/CommandRecognizer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RSPvMCompanionBot.Utility
+{
+    public abstract class CommandRecognizer
+    {
+        public static Dictionary<Regex, ICollection<string>> Recognize(ref string input, ICollection<Regex> recognizers)
+        {
+            Dictionary<Regex, ICollection<string>> matchedRegexes = new Dictionary<Regex, ICollection<string>>();
+            foreach (Regex regex in recognizers)
+            {
+                MatchCollection regexMatches = regex.Matches(input);
+                if (regexMatches.Count != 0)
+                {
+                    List<string> recognizedArgs = new List<string>();
+                    for (int i = 0; i < regexMatches.Count; i++)
+                    {
+                        string matchedString = regexMatches[i].ToString();
+                        recognizedArgs.Add(matchedString);
+                        input = input.Remove(input.IndexOf(matchedString), matchedString.Length);
+                    }
+                    matchedRegexes.Add(regex, recognizedArgs);
+                }
+            }
+
+            return matchedRegexes;
+        }
+
+    }
+}
