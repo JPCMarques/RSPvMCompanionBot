1ef905529862ab13f47aa4541a27e94f130d6256
JP Marques <JP Marques>
2017-05-10 17:03:59 +0100

    Updated generic Command class.

--- RSPvMCompanionBot/RSPvMCompanionBot.Commands/Command.cs
+++ RSPvMCompanionBot/RSPvMCompanionBot.Commands/Command.cs
@@ -16,9 +16,23 @@ namespace RSPvMCompanionBot.Commands
         {
             Root = root;
             ArgSpec = argSpec;
+            AssignArgumentSpecifications();
         }
 
         protected abstract void AssignArgumentSpecifications();
-        public abstract TReturn Execute();
+        protected abstract bool IsValidCommandCall();
+        protected abstract TReturn CommandLogic();
+
+        public TReturn Execute(string input)
+        {
+            if (IsValidCommandCall())
+            {
+                return CommandLogic();
+            }
+            else
+            {
+                throw new ArgumentException("The command call is not valid. Check if you passed all the arguments correctly.");
+            }
+        }
     }
 }
